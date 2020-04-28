f778138fedf1911132643b38e46a1de49054dc21
roubalsehgal <roubalsehgal2000@gmail.com>
2020-04-28 22:10:25 +0530

    Created first commit.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
+import { MainViewComponent } from './pages/main-view/main-view.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainViewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
